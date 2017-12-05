69f3556a29ca3f380f9b93b84e3434f35b63ba50
OlaOst <olaa81@gmail.com>
2017-12-05 19:15:20 +0100

    day 3 wip

--- source/app.d
+++ source/app.d
@@ -1,6 +1,45 @@
-import std.stdio;
+import std.stdio : writeln;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	
+}
+
+unittest
+{
+  //for (int i = 1; i < 27; i++)
+    //writeln("circle for ", i, ": ", steps(i));
+  
+  assert(steps(1) == 0);
+  assert(steps(12) == 3);
+}
+int steps(int input)
+{
+  if (input <= 1)
+    return 0;
+    
+  import std.math : sqrt;
+  
+  auto circle = (cast(int)sqrt(cast(float)input - 1) - 1) / 2 + 1;
+  
+  writeln("circle for ", input, ": ", circle);
+  
+  auto circleEnd = (circle*2+1)*(circle*2+1);
+  
+  writeln("circleEnd: ", circleEnd);
+  
+  auto sideLength = circle * 2;
+  
+  auto midpoints = [circleEnd - sideLength*1 + sideLength/2,
+                    circleEnd - sideLength*2 + sideLength/2,
+                    circleEnd - sideLength*3 + sideLength/2,
+                    circleEnd - sideLength*4 + sideLength/2];
+  
+  midpoints.writeln;
+  
+  auto offset = sideLength * 4 - input;
+
+  writeln("sidelength ", sideLength, ", offset ", offset);
+  
+  return circle;
 }
